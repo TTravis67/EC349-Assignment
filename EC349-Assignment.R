db9815e61060b816b4bc5e4c158918ccb95c4ea6
Travis Tan <tantravs6@gmail.com>
2023-11-21 13:21:39 +0000

    user data cleaned, in Elite and Friends

--- EC349-Assignment.R
+++ EC349-Assignment.R
@@ -1,25 +1,57 @@
 setwd("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/R projects/EC349-Assignment")
 
-#Pre-Processing Yelp Academic Data for the Assignment
+
+library(tidyverse)
 library(jsonlite)
 library(caret)
-
+library(lubridate)
 
 #Clear
 cat("\014")  
 rm(list=ls())
 
+#Load & save different data
+load("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/Assignment 1/Assignment/Small Datasets/yelp_user_small.Rda")
+
+summary(user_data_small$yelping_since)
+user_data_small[, 8]
+# Need to convert empty strings into NA's in Elite
+user_data_small <- user_data_small %>%
+  mutate(across(elite, ~na_if(., "")))
+#Need to convert None into NA's in friends
+user_data_small <- user_data_small %>%
+  mutate(across(friends, ~na_if(., "None")))
+user_data_small[, 8]
+# Need to convert 20,20 in yelping_since into 2020 in Elite
+user_data_small <- user_data_small %>%
+  mutate(elite = str_replace_all(elite, "20,20", "2020"))
+save(user_data_small, file = "user_data.Rdata")
 
 
-#Load Different Data
+
+
+
+
+
+load("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/Assignment 1/Assignment/Small Datasets/yelp_review_small.Rda")
 business_data <- stream_in(file("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/Assignment 1/Assignment/yelp_academic_dataset_business.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
-review_data  <- stream_in(file("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/Assignment 1/Assignment/yelp_academic_dataset_review.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
 checkin_data  <- stream_in(file("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/Assignment 1/Assignment/yelp_academic_dataset_checkin.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
-user_data <- stream_in(file("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/Assignment 1/Assignment/yelp_academic_dataset_user.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
 tip_data  <- stream_in(file("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/Assignment 1/Assignment/yelp_academic_dataset_tip.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
 
 
-set.seed(1)
-rev_data <- review_data[sample(nrow(review_data), 500000), ]
-save(rev_data, file = "rev_data.RData")
-load("C:/Users/Travis Tan/OneDrive - University of Warwick/EC349/R projects/EC349-Assignment-WIP/rev_data.RData")
+
+
+
+
+
+save(business_data, file = "business_data.RData")
+save(checkin_data, file = "checkin_data.Rdata")
+save(tip_data, file = "tip_data.Rdata")
+save(review_data_small, file = "review_data.Rdata")
+save(user_data_small, file = "user_data.Rdata")
+
+
+
+
+ymd_hms()
+
